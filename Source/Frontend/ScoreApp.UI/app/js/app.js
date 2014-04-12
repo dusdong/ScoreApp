e6a61c89ba813934762eba22f9f0d4cf3e99bdc2
JobaDiniz <jobertodinizjunior@gmail.com>
2014-04-12 13:27:10 -0300

    setting up routes

--- Source/Frontend/ScoreApp.UI/app/js/app.js
+++ Source/Frontend/ScoreApp.UI/app/js/app.js
@@ -1,14 +1,15 @@
 ﻿'use strict';
 
-var underscore = angular.module('underscore', []);
-underscore.factory('_', function () {
-    return window._;
-});
+var underscore = angular.module('underscore', [])
+    .factory('_', function () {
+        return window._;
+    });
 
-var scoreApp = angular.module('scoreApp', ['angularMoment', 'ui.bootstrap', 'underscore']);
-
-scoreApp.run(['$window',
-    function ($window) {
+var scoreApp = angular.module('scoreApp', ['ngRoute', 'angularMoment', 'ui.bootstrap', 'underscore'])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/Pontos', { templateUrl: '/app/partials/Scores.html', controller: 'ScoreController' });
+        $routeProvider.otherwise({ redirectTo: '/Pontos' });
+    }])
+    .run(['$window', function ($window) {
         $window.moment.lang('pt-BR');
-    }
-]);
+    }]);
