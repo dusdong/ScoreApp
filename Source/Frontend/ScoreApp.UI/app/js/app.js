d5e52718a52bef0c4e7bcbd3d587694f284150b7
JobaDiniz <jobertodinizjunior@gmail.com>
2014-04-15 21:55:55 -0300

    login process using UserApp API cloud service

--- Source/Frontend/ScoreApp.UI/app/js/app.js
+++ Source/Frontend/ScoreApp.UI/app/js/app.js
@@ -5,23 +5,17 @@ var underscore = angular.module('underscore', [])
         return window._;
     });
 
-var scoreApp = angular.module('scoreApp', ['ngRoute', 'angularMoment', 'ui.bootstrap', 'underscore'])
+var scoreApp = angular.module('scoreApp', ['ngRoute', 'angularMoment', 'UserApp', 'ui.bootstrap', 'underscore'])
     .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $routeProvider.when('/Votacao', { templateUrl: '/app/partials/Voting.html', controller: 'VotingController' });
-        $routeProvider.when('/Login', { templateUrl: '/app/partials/Login.html', controller: 'LoginController' });
-        $routeProvider.otherwise({ redirectTo: '/Votacao' });
+        $routeProvider.when('/Home', { templateUrl: '/app/partials/Home.html', public: true });
+        $routeProvider.when('/Login', { templateUrl: '/app/partials/Login.html', login: true });
+        $routeProvider.otherwise({ redirectTo: '/Home' });
         $locationProvider.html5Mode(true);
     }])
-    .run(['$window', '$rootScope', '$location', 'authentication', function ($window, $rootScope, $location, authentication) {
+    .run(['$window', '$rootScope', '$location', 'user', function ($window, $rootScope, $location, user) {
         $window.moment.lang('pt-BR');
-
-        $rootScope.$on('$routeChangeStart', function (event, next, current) {
-            if (!authentication.isLoggedIn()) {
-                if (next.templateUrl != "/app/partials/Login.html") {
-                    var returnUrl = $location.url();
-                    event.preventDefault();
-                    $location.path('/Login').search({ returnUrl: returnUrl });
-                }
-            }
+        user.init({
+            appId: '534db88a7c34a'
         });
     }]);
