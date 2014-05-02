1f58a27dc321a8b518aa1fe4977c4d004e4b3175
JobaDiniz <jobertodinizjunior@gmail.com>
2014-05-02 14:18:06 -0300

    apply ng-class in the buttons vote yes or no

--- Source/Frontend/ScoreApp.UI/app/js/controllers/VotingController.js
+++ Source/Frontend/ScoreApp.UI/app/js/controllers/VotingController.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-scoreApp.controller('VotingController', ['$scope', 'scoreData', 'toaster', 'user', function ($scope, scoreData, toaster, user) {
+scoreApp.controller('VotingController', ['$scope', 'scoreData', 'toaster', 'user', 'underscore', function ($scope, scoreData, toaster, user, _) {
     var promise = scoreData.getAll(false);
     $scope.scoresPromise = promise;
     promise.then(function (data) {
@@ -12,7 +12,14 @@ scoreApp.controller('VotingController', ['$scope', 'scoreData', 'toaster', 'user
     $scope.candidateIsLoggedUser = function (candidate) {
         return candidate.id == user.current.user_id;
     };
-
+    $scope.loggedUserVote = function (score) {
+        var userVoter = _.find(score.voters, function (voter) {
+            return voter.id == user.current.user_id;
+        });
+        if (userVoter == null)
+            return 'default';
+        return userVoter.isInFavor;
+    };
     $scope.vote = function (scoreId, vote) {
 
     };
