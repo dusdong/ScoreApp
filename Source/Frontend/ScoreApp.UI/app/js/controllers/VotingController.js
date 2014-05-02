4a41e9c3a4ae0b73428eae697c002a0e16733983
JobaDiniz <jobertodinizjunior@gmail.com>
2014-05-02 13:48:53 -0300

    disable vote button when candidate is logged user

--- Source/Frontend/ScoreApp.UI/app/js/controllers/VotingController.js
+++ Source/Frontend/ScoreApp.UI/app/js/controllers/VotingController.js
@@ -1,6 +1,6 @@
 ﻿'use strict';
 
-scoreApp.controller('VotingController', ['$scope', 'scoreData', 'toaster', function ($scope, scoreData, toaster) {
+scoreApp.controller('VotingController', ['$scope', 'scoreData', 'toaster', 'user', function ($scope, scoreData, toaster, user) {
     var promise = scoreData.getAll(false);
     $scope.scoresPromise = promise;
     promise.then(function (data) {
@@ -8,6 +8,10 @@ scoreApp.controller('VotingController', ['$scope', 'scoreData', 'toaster', funct
     }, function (data) {
         toaster.pop('error', '', 'Sorry, but an error occured');
     });
+
+    $scope.candidateIsLoggedUser = function (candidate) {
+        return candidate.id == user.current.user_id;
+    };
 
     $scope.vote = function (scoreId, vote) {
 
