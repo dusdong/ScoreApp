05bb3ea1587e4d58ee1ff7c928128ca05104325d
JobaDiniz <jobertodinizjunior@gmail.com>
2014-05-02 12:21:17 -0300

    fixing scoreCalculator

--- Source/Frontend/ScoreApp.UI/app/js/services/scoreCalculator.js
+++ Source/Frontend/ScoreApp.UI/app/js/services/scoreCalculator.js
@@ -3,11 +3,14 @@
 scoreApp.factory('scoreCalculator', ['underscore', function (_) {
     return {
         percentage: function (score) {
+            if (score.witnesses.length == 0)
+                return 0;
+
             var votersInFavor = _.filter(score.voters, function (voter) {
                 return voter.isInFavor == true;
             });
 
-            var percentage = (100 * ((votersInFavor.length * 100) / score.witnesses)) / 50; //50 é quantos % deve haver de votos a favor para que o ponto seja contabilizado (parametrizar).
+            var percentage = (100 * ((votersInFavor.length * 100) / score.witnesses.length)) / 50; //50 é quantos % deve haver de votos a favor para que o ponto seja contabilizado (parametrizar).
             return percentage > 100 ? 100 : percentage;
         }
     };
